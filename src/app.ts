889bf34fdd2096b81ab82341504d20adef1ee8f5
Vivek Nayyar <vivek.of.nayyar@gmail.com>
2023-12-31 16:46:33 +0700

    feat: code for youtube video

--- src/app.ts
+++ src/app.ts
@@ -1,69 +1,25 @@
-import { PDFLoader } from "langchain/document_loaders/fs/pdf";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { HNSWLib } from "langchain/vectorstores/hnswlib";
-import { RetrievalQAChain } from "langchain/chains";
-import { OpenAI } from "langchain/llms/openai";
+import OpenAI from "openai";
 import * as dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
-const loader = new PDFLoader("src/documents/budget_speech.pdf");
-
-const docs = await loader.load();
-
-// splitter function
-const splitter = new RecursiveCharacterTextSplitter({
-  chunkSize: 1000,
-  chunkOverlap: 20,
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
-// created chunks from pdf
-const splittedDocs = await splitter.splitDocuments(docs);
+async function main() {
+  try {
+    const response = await openai.audio.transcriptions.create({
+      model: "whisper-1",
+      response_format: "srt",
+      file: fs.createReadStream("src/audios/2023-12-18 01-33-22.mp3"),
+    });
 
-const embeddings = new OpenAIEmbeddings();
+    console.log(response);
+  } catch (e) {
+    console.log(e);
+  }
+}
 
-const vectorStore = await HNSWLib.fromDocuments(
-  splittedDocs,
-  embeddings
-);
-
-const vectorStoreRetriever = vectorStore.asRetriever();
-const model = new OpenAI({
-  modelName: 'gpt-3.5-turbo'
-});
-
-const chain = RetrievalQAChain.fromLLM(model, vectorStoreRetriever);
-
-const question = 'What is the theme of G20?';
-
-const answer = await chain.call({
-  query: question
-});
-
-console.log({
-  question,
-  answer
-});
-
-const question1 = 'What is the budget allocated for railways?';
-const answer1 = await chain.call({
-  query: question1
-});
-
-console.log({
-  question: question1,
-  answer: answer1
-});
-
-const question2 = 'what is PM Matsya Sampada Yojana?'
-const answer2 = await chain.call({
-  query: question2
-});
-console.log({
-  question: question2,
-  answer: answer2
-});
-
-
-
+main();
